e6a28b8daf1444d74f0706c0ab6403e56f31c5e6
“wzy <1757629766@qq.com>
2019-03-19 14:55:58 +0800

    高阶函数应用的简单例子

---
+++ 1.js
@@ -0,0 +1,15 @@
+//时间函数的应用
+// setTimeout(function(){
+//     console.log("Hello World")
+// },1000)
+// setInterval(function(){
+//     console.log("Hello World2")
+// },1000)
+
+///ajax
+// $.get('/api/getTime',function(){
+//     console.log('获取成功')
+// })
+
+//数组中的应用
+// some()、every()、filter()、map()、foreach()
